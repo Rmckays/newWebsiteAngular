31fd7954025f47b74bbcd06e5c1a2a1b4b185181
Rustyn Simmons <Rustyn@rustynsimmons.com>
2020-05-31 12:06:00 -0700

    Add navigation component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {CarouselModule} from "ngx-bootstrap/carousel";
+import { NavigationComponent } from './navigation/navigation.component';
+import {CollapseModule} from "ngx-bootstrap/collapse";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CollapseModule,
+    BrowserAnimationsModule,
+    CarouselModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
